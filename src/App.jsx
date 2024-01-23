f330d30dc3768710712d2c3bef7bea848febd3da
Piyush-Das <piyushdas3110@gmail.com>
2024-01-24 01:54:46 +0530

    added loadmoroe project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import ImageSlider from "./Components/ImageSlider/ImageSlider";
-import StarRating from "./Components/Star/StarRating";
-
+// import ImageSlider from "./Components/ImageSlider/ImageSlider";
+// import StarRating from "./Components/Star/StarRating";
+import Index from "./Components/LoadMoreData";
 function App() {
   return (
     <>
       {/* <StarRating noOfStars={10} />  */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={5} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={5} /> */}
+      <Index />
     </>
   );
 }
